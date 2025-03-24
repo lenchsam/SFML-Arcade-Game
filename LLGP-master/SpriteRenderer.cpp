47051166d7f1904a67ed07d22d8e022b3b692247
Sam <114984800+lenchsam@users.noreply.github.com>
2025-03-24 12:17:42 +0000

    sprite renderer fixed error

--- LLGP-master/SpriteRenderer.cpp
+++ LLGP-master/SpriteRenderer.cpp
@@ -1,10 +1,18 @@
 #include "SpriteRenderer.h"
 
 namespace LLGP {
-	SpriteRenderer::SpriteRenderer(GameObject* owner) {
+	SpriteRenderer::SpriteRenderer(GameObject* owner) : m_sprite(m_texture) {
 		_GameObject = owner;
 	}
-	//void SpriteRenderer::SetSprite(sf::Sprite sprite) {
-	//	m_sprite = sprite;
-	//}
+
+	void SpriteRenderer::SetSprite(sf::Sprite sprite) {
+		m_sprite = sprite;
+	}
+
+	void SpriteRenderer::LoadTexture(std::string filePath) {
+		if (m_texture.loadFromFile(filePath))
+		{
+			//m_Sprite
+		}
+	}
 }
--- LLGP-master/SpriteRenderer.h
+++ LLGP-master/SpriteRenderer.h
@@ -2,17 +2,16 @@
 #include "Component.h"
 #include <SFML/Graphics.hpp>
 
-
-
 namespace LLGP {
 	class SpriteRenderer : public Component
 	{
 	public:
 		SpriteRenderer(GameObject* owner);
+		void LoadTexture(std::string filePath);
 		void SetSprite(sf::Sprite sprite);
 	private:
-		//sf::Texture m_texture;
-		//sf::Sprite m_sprite;
+		sf::Texture m_texture;
+		sf::Sprite m_sprite;
 	};
 }
 
