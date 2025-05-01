1de134c344ba0a03d9c2fcf9abc0f3c970ae3668
Sam <114984800+lenchsam@users.noreply.github.com>
2025-05-01 22:38:57 +0100

    displaying high score

--- LLGP-master/MainMenuState.cpp
+++ LLGP-master/MainMenuState.cpp
@@ -2,6 +2,9 @@
 #include "UI.h"
 #include "Button.h"
 #include "GameplayState.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 namespace LLGP {
 	void MainMenuState::OnEnter(StateManager* manager) {
@@ -9,6 +12,23 @@ namespace LLGP {
 		playButton = new Button(sf::FloatRect({ 475.f, 490.f }, { 50.f, 25.f }));
 		quitButton = new Button(sf::FloatRect({ 475.f, 550.f }, { 50.f, 25.f }));
 		UI::AddText("Quit", { 500.f, 560.f }, 30, sf::Color::White, true);
+
+		//high score text
+		std::vector<std::string> scores;
+		std::ifstream file("highscores.txt");
+		if (file.is_open()) {
+			std::string line;
+			while (std::getline(file, line)) {
+				scores.push_back(line);
+			}
+			file.close();
+		}
+
+		UI::AddText("High Scores", {200.f, 300.f}, 30, sf::Color::White, true);
+
+		for (int i = 0; i < scores.size(); i++) {
+			UI::AddText(scores[i], {200.f, (300.f + (i * 25.f))}, 30, sf::Color::White, true);
+		}
 	}
 	void MainMenuState::OnExit() {
 		UI::ClearText();
